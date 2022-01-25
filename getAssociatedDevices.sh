c64f4cfe33617b3df944238e54f86a276123f367
bernhara <raphael.bernhard@orange.fr>
2022-01-25 16:44:32 +0100

    send delta as long type

--- getAssociatedDevices.sh
+++ getAssociatedDevices.sh
@@ -122,7 +122,7 @@ makeStatLine ()
     do
 	mib_parameter_value=$( getMibParameter "${mib_data_for_mac}" "${mib_parameter_name}" )
 	delta=$( getDeltaForVal "${mac_address}" "${mib_parameter_value}" "${mib_parameter_name}" )
-	stat_line_extends=${stat_line_extends}', "X_'${mib_parameter_name}'_delta":"'${delta}'"'
+	stat_line_extends=${stat_line_extends}', "X_'${mib_parameter_name}'_delta":'${delta}''
 
     done
 
