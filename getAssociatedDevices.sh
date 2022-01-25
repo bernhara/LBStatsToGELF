57b2d507f0bde9258d91173b0ee418ce1d4fd5c4
bernhara <raphael.bernhard@orange.fr>
2022-01-25 12:05:57 +0100

    create function to make GELK line

--- getAssociatedDevices.sh
+++ getAssociatedDevices.sh
@@ -74,23 +74,49 @@ computeDeltaForVal ()
     cmd="${last_values_array_name}"'=( '"${last_values_array[@]}"' )'
     eval ${cmd}
 
-    echo "${last_values_array[@]}" 1>&2
-    echo "${_last_value_Rx_Retransmissions[@]}" 1>&2
-
     # return the new array
     echo ${delta}
 }
 
 
-computeDeltaForVal 'A8:B8:6E:81:37:4E' 10 "_last_value_Rx_Retransmissions"
-echo "=================================== ${_known_mac_addresses} ${_last_value_Rx_Retransmissions[@]}" 1>&2
-computeDeltaForVal A8:B8:6E:81:37:4E 12 "_last_value_Rx_Retransmissions"
-echo "=================================== ${_known_mac_addresses} ${_last_value_Rx_Retransmissions[@]}" 1>&2
-computeDeltaForVal A8:B8:6E:81:37:4E 9 "_last_value_Rx_Retransmissions"
-echo "=================================== ${_known_mac_addresses} ${_last_value_Rx_Retransmissions[@]}" 1>&2
-exit 1
+# delta=$( computeDeltaForVal 'A8:B8:6E:81:37:4E' 10 "_last_value_Rx_Retransmissions" )
+# echo "=================================== ${_known_mac_addresses} ${_last_value_Rx_Retransmissions[@]}" 1>&2
+# computeDeltaForVal A8:B8:6E:81:37:4E 12 "_last_value_Rx_Retransmissions"
+# echo "=================================== ${_known_mac_addresses} ${_last_value_Rx_Retransmissions[@]}" 1>&2
+# computeDeltaForVal A8:B8:6E:81:37:4E 9 "_last_value_Rx_Retransmissions"
+# echo "=================================== ${_known_mac_addresses} ${_last_value_Rx_Retransmissions[@]}" 1>&2
+
+makeStatLine ()
+{
+    device="$1"
+    lb_interface="$2"
+    
+    stat_line=$( echo "${device}" | jq -c '.' )
+    mac_address=$( echo "${stat_line}" | jq '."MACAddress"' | sed -e 's/^\"//' -e 's/\"$//' )
+    model_for_mac_address=$( ${SYSBUS} -model "Hosts.Host.${mac_address}" )
+
+    model_IPAddress=$( getMibParameter "${model_for_mac_address}" 'IPAddress' )
+    model_HostName=$( getMibParameter "${model_for_mac_address}" 'HostName' )
+    model_XORANGECOM_InterfaceTypes=$( getMibParameter "${model_for_mac_address}" 'X_ORANGE-COM_InterfaceType' )
 
 
+    stat_line_extends='"version": "1.1"'
+    stat_line_extends=${stat_line_extends}', "host":"s-ku2raph"'
+    stat_line_extends=${stat_line_extends}', "short_message":"LB wifi '${LOOP_DELAY}' stat for '${model_HostName}'"'
+    stat_line_extends=${stat_line_extends}', "lb_interface":"'${lb_interface}'"'
+
+    stat_line_extends=${stat_line_extends}', "HostName":"'${model_HostName}'"'
+    stat_line_extends=${stat_line_extends}', "IPAddress":"'${model_IPAddress}'"'
+    stat_line_extends=${stat_line_extends}', "X_ORANGE-COM_InterfaceTypes":"'${model_XORANGECOM_InterfaceTypes}'"'
+
+    json_extends="{ ${stat_line_extends} }"
+    echo ${json_extends}
+
+    GELF_stat_to_send=$( echo "${stat_line}" | jq -c ". += ${json_extends}" )
+    echo "${GELF_stat_to_send}"
+	
+    echo "${GELF_stat_line}"
+}
 
 
 while true
@@ -110,39 +136,17 @@ do
     for d in ${WIFI24G_DEVICEs}
     do
 
-	stat_line=$( echo "${d}" | jq -c '. += {"version": "1.1", "host":"s-ku2raph", "short_message":"LB", "lb_interface":"wl0"}' )
-	
-	echo -n "${stat_line}" | nc -w0 -v -u t3620 12202
+	GELF_stat_line=$( makeStatLine "${d}" "wl0" )
+	echo -n "${GELF_stat_line}" | nc -w0 -v -u t3620 12202
+
     done
 
     for d in ${WIFI5G_DEVICEs}
     do
 
-	stat_line=$( echo "${d}" | jq -c '.' )
-	mac_address=$( echo "${stat_line}" | jq '."MACAddress"' | sed -e 's/^\"//' -e 's/\"$//' )
-	model_for_mac_address=$( ${SYSBUS} -model "Hosts.Host.${mac_address}" )
-
-	model_IPAddress=$( getMibParameter "${model_for_mac_address}" 'IPAddress' )
-	model_HostName=$( getMibParameter "${model_for_mac_address}" 'HostName' )
-	model_XORANGECOM_InterfaceTypes=$( getMibParameter "${model_for_mac_address}" 'X_ORANGE-COM_InterfaceType' )
-
-
-	stat_line_extends='"version": "1.1"'
-	stat_line_extends=${stat_line_extends}', "host":"s-ku2raph"'
-	stat_line_extends=${stat_line_extends}', "short_message":"LB wifi '${LOOP_DELAY}' stat for '${model_HostName}'"'
-	stat_line_extends=${stat_line_extends}', "lb_interface":"eth6"'
-
-	stat_line_extends=${stat_line_extends}', "HostName":"'${model_HostName}'"'
-	stat_line_extends=${stat_line_extends}', "IPAddress":"'${model_IPAddress}'"'
-	stat_line_extends=${stat_line_extends}', "X_ORANGE-COM_InterfaceTypes":"'${model_XORANGECOM_InterfaceTypes}'"'
-
-	json_extends="{ ${stat_line_extends} }"
-	echo ${json_extends}
-
-	GELF_stat_to_send=$( echo "${stat_line}" | jq -c ". += ${json_extends}" )
-	echo "${GELF_stat_to_send}"
-	
+	GELF_stat_line=$( makeStatLine "${d}" "eth6" )
 	echo -n "${GELF_stat_line}" | nc -w0 -v -u t3620 12202
+
     done
 
     sleep ${LOOP_DELAY}
